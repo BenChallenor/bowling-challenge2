e90e6841a8891f9442e57a08436384df35810acf
Ben <ben.challenor@btopenworld.com>
2018-03-04 21:46:00 +0000

    refactored test

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,19 +1,27 @@
 describe('Bowling game', function() {
 
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame();
+  });
+
 
   it('can roll gutter game', function() {
-    var frame = new Frame();
-    for (var i = 0; i < 20; i++) {
-      frame.roll(0);
-    }
+    rollMany(0, 20);
     expect(frame.score()).toBe(0);
   });
 
   it('can roll all ones', function() {
-    var frame = new Frame();
-    for (var i = 0; i < 20; i++) {
-      frame.roll(1);
-    }
+    rollMany(1, 20);
     expect(frame.score()).toBe(20);
   });
+
+  var rollMany = function(pins, rolls) {
+    for (var i = 0; i < 20; i++) {
+      frame.roll(pins);
+    }
+  };
+
+
 });
